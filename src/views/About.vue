005f59580967b94315fe7104fdbb95ff1c001d56
hrj <a2390266>
2020-08-12 00:42:12 +0800

    Vue框架加Vant

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <div class="about">
+            <h1>This is an about page</h1>
+        </div>
+        <vue-qr ref="Qrcode"
+                :text="qrCodeConfig.text"
+                :download="downloadFilename"
+                :margin="10"
+                :size="200"
+                :dotScale="qrCodeConfig.dotScale"
+        >
+        </vue-qr>
+        <div>
+            <van-button type="info">信息按钮</van-button>
+        </div>
+    </div>
 </template>
+<script>
+    import VueQr from "vue-qr";
+    import { Button } from 'vant';
+
+    export default {
+        name: "About",
+        components: {
+            VueQr,
+            "van-button":Button,//对应<van-button>标签
+        },
+        data() {
+            return {
+                qrCodeConfig: {
+                    text: "https://www.baidu.com",
+                    dotScale: 0.9
+//colorDark: '#663300'
+                },
+                downloadFilename: ""
+            }
+        }
+    }
+</script>
